4543b9250c6a69f8726590995331037fff2da68b
AntonellaLV <118840064+AntonellaLV@users.noreply.github.com>
2023-09-23 22:43:00 -0300

    Delete Programacion Imperativa directory

--- Programacion Imperativa/TrianguloDeAsteriscos.java
+++
@@ -1,9 +0,0 @@
-public class TrianguloDeAsteriscos {
-    public static void main(String[] args) {
-        System.out.println("*");
-        System.out.println("**");
-        System.out.println("***");
-        System.out.println("****");
-        System.out.println("*****");
-    }
-}
